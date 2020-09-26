8ebee443d8dcf938f3a0aed7176e1589e5b121ee
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-09-26 16:59:44 +0300

    add env elements

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+"development": {
+    "username": "root",
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "mysql"
+},
+"test": {
+    "username": "root",
+    "password": process.env.PASSWORD,
+    "database": "database_test",
+    "host": process.env.HOST,
+    "dialect": "mysql"
+},
+"production": {
+    "username": "root",
+    "password": process.env.PASSWORD,
+    "database": "database_production",
+    "host": process.env.HOST,
+    "dialect": "mysql"
+}
+};
+
